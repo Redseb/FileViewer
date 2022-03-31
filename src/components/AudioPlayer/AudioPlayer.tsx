60938a594a9fff365128b67d380d5ed7469670b4
Redseb <mikolajzyzanski@gmail.com>
2022-03-31 09:29:32 +0200

    Refactored styling in AudioPlayer

--- src/components/AudioPlayer/AudioPlayer.tsx
+++ src/components/AudioPlayer/AudioPlayer.tsx
@@ -6,18 +6,6 @@ import { useDropzone } from 'react-dropzone';
 //types
 import { File } from '../../types/Files';
 
-const baseStyle = {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#ead2ac',
-    borderColor: '#30343f',
-    borderWidth: 2,
-    borderStyle: 'solid',
-    height: '50%'
-} as React.CSSProperties;
-
 const activeDragStyle = {
     backgroundColor: '#f7e3c4',
     borderStyle: 'dashed',
@@ -43,16 +31,15 @@ const AudioPlayer = () => {
     const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
 
     const style = useMemo(() => ({
-        ...baseStyle,
         ...(isDragActive ? activeDragStyle : {})
     }), [
         isDragActive
     ]);
 
     return (
-        <div {...getRootProps({ style })} >
+        <div {...getRootProps({ style })} className="AudioPlayer">
             <input {...getInputProps()} />
-            <p>{file.name === "" ? 'No audio file selected. Try dragging and dropping a file.' : file.name}</p>
+            <p>{file.name === "" ? isDragActive ? <b>Drop here!</b> : 'No audio file selected. Try dragging and dropping a file.' : file.name}</p>
 
             <ReactAudioPlayer
                 src={file.url}
